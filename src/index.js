67e84a192966a8d0e91b4d4dcab2f53a204a7879
gllm <special.geeee@gmail.com>
2020-07-31 03:34:09 +0200

    updated index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SpeechSynthesisExample from './useSpeechSynthesis';
+import { GlobalStyles, Row } from './shared';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => (
+  <div>
+    <GlobalStyles />
+    <Row>
+      <SpeechSynthesisExample />
+    </Row>
+  </div>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(<App />, document.getElementById('root'));

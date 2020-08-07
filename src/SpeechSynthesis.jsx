9eef2b7227495e0ea4d91f7989c189568e75df03
gllm <special.geeee@gmail.com>
2020-08-07 03:04:46 +0200

    updated SpeechSynthesis component

--- src/SpeechSynthesis.jsx
+++ src/SpeechSynthesis.jsx
@@ -2,14 +2,17 @@ import React, { useState } from 'react';
 import { useSpeechSynthesis } from 'react-speech-kit';
 import { Container } from './shared';
 
-const SpeechSynthesis = () => {
-  const [text, setText] = useState('');
+const SpeechSynthesis = (props) => {
+  let textArea = props.textArea.current;
+
+  let [text, setText] = useState('');
   const [rate, setRate] = useState(0.8);
   const [pitch, setPitch] = useState(1.3);
   const [voiceIndex, setVoiceIndex] = useState(null);
   const onEnd = () => {
     // do something here after speaking has finished
   };
+
   const { speak, cancel, speaking, supported, voices } = useSpeechSynthesis({
     onEnd,
   });
@@ -23,6 +26,19 @@ const SpeechSynthesis = () => {
     flexDirection: 'column',
     marginBottom: 12,
   };
+
+  let showParameters = false;
+  let parametersEl = document.getElementsByClassName("parameters-panel");
+
+  function toggleParameters() {
+    showParameters = !showParameters;
+
+    if (showParameters) {
+      parametersEl[0].style.visibility = "visible";
+    } else {
+      parametersEl[0].style.visibility = "hidden";
+    }
+  }
 
   return (
     <Container>
@@ -57,8 +73,8 @@ const SpeechSynthesis = () => {
                   </option>
                 ))}
               </select> */}
-              <div className="text-area">
-                <label className="message-label" htmlFor="message">テキスト</label>
+              <div>
+                {/* <label className="message-label" htmlFor="message">テキスト</label>
                 <textarea
                   id="message"
                   name="message"
@@ -67,7 +83,7 @@ const SpeechSynthesis = () => {
                   onChange={(event) => {
                     setText(event.target.value);
                   }}
-                />
+                /> */}
                 {speaking ? (
                   <button className="button-stop" type="button" onClick={cancel}>
                     STOP
@@ -77,6 +93,7 @@ const SpeechSynthesis = () => {
                     className="button-speak"
                     type="button"
                     onClick={() => {
+                        text = textArea.state.text;
                         speak({ text, voice, rate, pitch });
                       }
                     }
@@ -85,61 +102,66 @@ const SpeechSynthesis = () => {
                   </button>
                 )}
               </div>
-              <div className="parameters-panel">
-                <div className="voice-panel">
-                  <label htmlFor="voiceScript">SCRIPT VOICE</label>
-                  <select
-                    id="voiceScript"
-                    name="voiceScript"
-                    value={voiceIndex || ''}
-                    onChange={(event) => {
-                      setVoiceIndex(event.target.value);
-                      voice = voices[event.target.value];
-                      console.log(voice)
-                    }}
-                  >
-                    <option value="">Default</option>
-                    {voices.map((option, index) => (
-                      <option key={option.voiceURI} value={index}>
-                        {`${option.lang} - ${option.name}`}
-                      </option>
-                    ))}
-                  </select>
+              <div className="parameters-wrapper">
+                <div className="parameters-toggle">
+                  <p onClick={(event) => {toggleParameters()}}>+</p>
                 </div>
-                <div className="slider-area">
-                  <div className="rate" style={styleContainerRatePitch}>
-                    <div style={styleFlexRow}>
-                      <label htmlFor="rate">Rate: </label>
-                      <div className="rate-value">{rate}</div>
-                    </div>
-                    <input
-                      type="range"
-                      min="0.0"
-                      max="2"
-                      defaultValue="0.8"
-                      step="0.1"
-                      id="rate"
+                <div className="parameters-panel">
+                  <div className="voice-panel">
+                    <label htmlFor="voiceScript">SCRIPT VOICE</label>
+                    <select
+                      id="voiceScript"
+                      name="voiceScript"
+                      value={voiceIndex || ''}
                       onChange={(event) => {
-                        setRate(event.target.value);
+                        setVoiceIndex(event.target.value);
+                        voice = voices[event.target.value];
+                        console.log(voice)
                       }}
-                    />
+                    >
+                      <option value="">Default</option>
+                      {voices.map((option, index) => (
+                        <option key={option.voiceURI} value={index}>
+                          {`${option.lang} - ${option.name}`}
+                        </option>
+                      ))}
+                    </select>
                   </div>
-                  <div className="pitch" style={styleContainerRatePitch}>
-                    <div style={styleFlexRow}>
-                      <label htmlFor="pitch">Pitch: </label>
-                      <div className="pitch-value">{pitch}</div>
+                  <div className="slider-area">
+                    <div className="rate" style={styleContainerRatePitch}>
+                      <div style={styleFlexRow}>
+                        <label htmlFor="rate">Rate: </label>
+                        <div className="rate-value">{rate}</div>
+                      </div>
+                      <input
+                        type="range"
+                        min="0.0"
+                        max="2"
+                        defaultValue="0.8"
+                        step="0.1"
+                        id="rate"
+                        onChange={(event) => {
+                          setRate(event.target.value);
+                        }}
+                      />
                     </div>
-                    <input
-                      type="range"
-                      min="0"
-                      max="2"
-                      defaultValue="1.3"
-                      step="0.1"
-                      id="pitch"
-                      onChange={(event) => {
-                        setPitch(event.target.value);
-                      }}
-                    />
+                    <div className="pitch" style={styleContainerRatePitch}>
+                      <div style={styleFlexRow}>
+                        <label htmlFor="pitch">Pitch: </label>
+                        <div className="pitch-value">{pitch}</div>
+                      </div>
+                      <input
+                        type="range"
+                        min="0"
+                        max="2"
+                        defaultValue="1.3"
+                        step="0.1"
+                        id="pitch"
+                        onChange={(event) => {
+                          setPitch(event.target.value);
+                        }}
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
